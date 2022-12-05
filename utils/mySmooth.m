5c66b2d70b98fdcbdb6fc77f1706a66bcafe58af
MunibH <munibh15@gmail.com>
2022-12-05 12:52:33 -0500

    bc smoothing notes

--- utils/mySmooth.m
+++ utils/mySmooth.m
@@ -1,14 +1,32 @@
 function [out,kernsd] = mySmooth(x, N, varargin)
-% operates on first dimension only
-% gaussian kernel with window size N, std dev sigma
+% operates on first dimension only - make sure first dim is time
+% gaussian kernel with window size N
 % varargin:
 %   'reflect' - reflect N elements from beginning of time series across
 %               0 index
 %   'zeropad' - pad N zeros across 0 index
+%   'none'    - don't handle boundary conditions
 
 % returns:
-% - out: filtered data
+% - out: filtered data, same size as x
 % - kernsd: std dev of gaussian kernel
+
+% % example usage:
+% x = 5*sin(1:0.01:100)';
+% y = x + randn(size(x));
+% N = 51;
+% out = mySmooth(y,N,'reflect');
+% 
+% figure;
+% subplot(3,1,1); plot(x);
+% xlim([-100 200])
+% subplot(3,1,2); plot(y)
+% xlim([-100 200])
+% subplot(3,1,3); plot(out); %hold on; plot(out,'.','MarkerSize',6)
+% xlim([-100 200])
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 if N == 0 || N == 1 % no smoothing
     out = x;
@@ -24,18 +42,10 @@ if nargin > 2
     % handle a boundary condition
     bctype = varargin{1};
     if strcmpi(bctype,'reflect')
-        try
         x_filt = cat(1,x(1:N,:),x);
-        catch
-            'a'
-        end
         trim = N + 1;
     elseif strcmpi(bctype,'zeropad')
-        try
         x_filt = cat(1,zeros(N,1),x);
-        catch
-            'a'
-        end
         trim = N + 1;
     elseif strcmpi(bctype,'none')
         x_filt = x;
